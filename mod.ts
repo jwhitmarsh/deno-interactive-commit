21ad6fd34d63d14489211ae3577f74ce47f870c8
James Whitmarsh <jwhitmarsh@dresources.com>
2020-06-05 18:32:02 +0100

    feat(mod): add logic to load config file with custom scopeFormatters

--- mod.ts
+++ mod.ts
@@ -1,7 +1,47 @@
 import { Input, Select } from "https://deno.land/x/cliffy/prompt.ts";
-import { basename, extname } from "https://deno.land/std/path/mod.ts";
+import {
+  basename,
+  extname,
+  sep,
+  dirname,
+} from "https://deno.land/std/path/mod.ts";
+import { Config } from "https://raw.githubusercontent.com/eankeen/config/master/mod.ts";
+import { pascalCase } from "https://deno.land/x/case/mod.ts";
 
-const { exit, run } = Deno;
+const { exit, run, cwd } = Deno;
+
+interface DgcmConfig {
+  scopeFormatters?: DgcmScopeFormatter[];
+}
+
+interface DgcmScopeFormatter {
+  test: string;
+  includeParentDirs: number;
+  case: "camel" | "pascal";
+}
+
+interface ScopeFormatResult {
+  formattedScope: string;
+  alreadyFormatted: boolean;
+}
+
+const readPermission = await Deno.permissions.request({
+  name: "read",
+  path: "./",
+});
+if (readPermission.state !== "granted") {
+  console.error("Read permission required");
+  exit(1);
+}
+
+const config: DgcmConfig | undefined = await Config.load({
+  file: "dgcm",
+  searchDir: cwd(),
+});
+if (!config) {
+  console.log("config is 'undefined' when no config files were found");
+  exit(1);
+}
 
 const p = run({
   cmd: ["git", "diff", "--name-only", "--cached"],
@@ -21,10 +61,32 @@ if (code !== 0) {
 
 const diffOutput = await p.output();
 const stagedFilesStr = new TextDecoder().decode(diffOutput);
+
 const stagedFilesArray = stagedFilesStr
   .split("\n")
-  .filter((fileName) => fileName.length)
-  .map((fileName) => basename(fileName, extname(fileName)));
+  .filter((filePath) => filePath.length)
+  .map((filePath) => {
+    const fileName = basename(filePath, extname(filePath));
+    let fileNameForList = fileName;
+
+    if (config?.scopeFormatters?.length) {
+      const scopeFormatResult = config?.scopeFormatters?.reduce(
+        (scope, ScopeFormatter) => {
+          scope = applyScopeFormatter(
+            scope,
+            filePath,
+            fileName,
+            ScopeFormatter
+          );
+          return scope;
+        },
+        { formattedScope: "", alreadyFormatted: false }
+      );
+      fileNameForList = scopeFormatResult.formattedScope;
+    }
+
+    return fileNameForList;
+  });
 
 if (!stagedFilesArray.length) {
   console.error("No files have been staged");
@@ -74,3 +136,46 @@ console.log(`\nCommit message: '${resolvedMessage}'\n`);
 await run({
   cmd: ["git", "commit", "-m", resolvedMessage],
 }).status();
+
+function applyScopeFormatter(
+  scope: ScopeFormatResult,
+  filePath: string,
+  fileName: string,
+  { test, includeParentDirs }: DgcmScopeFormatter
+): ScopeFormatResult {
+  const regex = new RegExp(test);
+  if (!regex.test(fileName)) {
+    return {
+      formattedScope: fileName,
+      alreadyFormatted: scope.alreadyFormatted,
+    };
+  }
+
+  if (scope.alreadyFormatted) {
+    console.error(
+      "mutliple scopeFormatters apply to filename which is not currently supported"
+    );
+    console.error(filePath);
+    exit(1);
+  }
+
+  const pathParts = dirname(filePath).split(sep);
+  if (includeParentDirs > pathParts.length) {
+    console.error(
+      `config.includeParentDirs "${includeParentDirs}" is greater than the directory depth`
+    );
+    console.error(filePath);
+    exit(1);
+  }
+
+  const requiredPathParts = pathParts.slice(
+    pathParts.length - includeParentDirs
+  );
+  // join with underscore so we can apply case formatter later
+  const requiredPathPartsString = requiredPathParts.join("_");
+
+  return {
+    formattedScope: pascalCase(`${requiredPathPartsString}_${fileName}`),
+    alreadyFormatted: true,
+  };
+}
